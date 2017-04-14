0f8d66925554bdd48c3d04cd9d35f8afbecf2744
Juan Becker <wolli.becker@gmail.com>
2017-04-14 13:06:28 +0200

    Rewrite test suite comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,38 +9,22 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        // Make sure the allFeeds variable has been defined and that it is not empty.
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        // Ensure every feed has a URL defined and that the URL is not empty.
         it('have URLs', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeDefined();
             });
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        // Ensure every feed has a name defined and that the name is not empty.
         it('have names', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
@@ -48,22 +32,13 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function(argument) {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        // Ensure the menu element is hidden by default.
         it('is hidden by default', function() {
             expect($('body')).toHaveClass('menu-hidden');
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        // Ensure the menu changes visibility when the menu icon is clicked.
         it('toggles visibility when the menu icon gets clicked', function() {
             var $body = $('body');
             var $menuIconLink = $('.menu-icon-link');
@@ -75,35 +50,29 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
-         beforeEach(function(done) {
-            loadFeed(0, done);
-         });
-
-         it('have at least one entry', function(done) {
-            var $entries = $(".feed").find(".entry");
-            expect($entries.length).toBeGreaterThan(0);
-            done();
-         });
-    });
-
-    /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
         beforeEach(function(done) {
             loadFeed(0, done);
         });
 
+        /* Ensure when the loadFeed function is called and completes its work,
+         * there is at least a single .entry element within the .feed container.
+         */
+        it('have at least one entry', function(done) {
+            var $entries = $(".feed").find(".entry");
+            expect($entries.length).toBeGreaterThan(0);
+            done();
+        });
+    });
+
+    describe('New Feed Selection', function() {
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        /* Ensure when a new feed is loaded by the loadFeed function that the
+         * content actually changes.
+         */
         it('changes the content', function(done) {
             var $initialFeedContent = $(".feed").html();
 
@@ -115,21 +84,20 @@ $(function() {
         });
     });
 
-    /* Attempts to request content from inexistent feeds generate
-     * an error message.
-     */
     describe('Unknown Feed Request', function() {
         beforeEach(function(done) {
             spyOn(window, 'alert');
             loadFeed(10, done);
         });
 
+        /* Ensure an error message is generated when content is requested from
+         * inexistent feeds.
+         */
         it('displays an error message', function() {
             expect(window.alert).toHaveBeenCalledWith("Error: The requested feed doesn't exist.");
         });
     });
 
-    // Show error message when AJAX fails
     describe('Failed Feed request', function() {
         beforeEach(function(done) {
             jasmine.Ajax.install();
@@ -139,6 +107,8 @@ $(function() {
             request.respondWith({status: 503});
         });
 
+        /* Ensure an error message is generated when AJAX fails.
+         */
         it('displays an error message', function() {
             expect(window.alert).toHaveBeenCalledWith("Error: The content failed to load. Please check your Internet connection.");
         });
